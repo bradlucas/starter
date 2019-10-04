01459a0500208d889f01fcd02a8a288dbacd667e
Brad Lucas <brad@beaconhill.com>
2019-10-04 11:59:10 -0400

    command line support

--- src/starter/core.clj
+++ src/starter/core.clj
@@ -1,6 +1,37 @@
-(ns starter.core)
+(ns starter.core
+  (:require [clojure.pprint :as pp]
+            [clojure.string :as str]
+            [clojure.tools.cli :refer [parse-opts]])
+  (:gen-class))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+
+(def cli-options
+  [["-w" "--web" "Start web app"]
+   ["-h" "--help" "Show help usage"]])
+
+(defn usage [summary]
+  (->> ["usage: starter.jar [options]"
+        "options:"
+        summary
+        ""
+        "Examples:"
+        "java -jar starter.jar -w           ;; start web app"
+        ""
+        ]
+       (str/join \newline)))
+
+(defn initialize []
+  ;; (config/load-config)
+  ;; (mount/start)
+)
+
+
+(defn -main [& args]
+  (initialize)
+  (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+      (cond
+        (:help options) (println (usage summary))
+        :else (println "start web server") ;; (jetty/run-jetty handler/app {:port 4003})
+        )))
+
+
